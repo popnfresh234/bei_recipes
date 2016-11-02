39b743c6fa302ac885e5bc156affc38ebbdb5918
Alexander Holliday <Alexander Holliday>
2016-11-01 22:28:15 -0700

    feat: Recipe class implements UUID identifier

--- app/src/main/java/com/dmtaiwan/alexander/beirecipes/data/Recipe.java
+++ app/src/main/java/com/dmtaiwan/alexander/beirecipes/data/Recipe.java
@@ -5,6 +5,7 @@ import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Alexander on 10/28/2016.
@@ -19,13 +20,13 @@ public class Recipe implements Parcelable {
         Recipe recipe = new Recipe();
         recipe.name = name;
         recipe.drawableId = 0;
+        recipe.id = UUID.randomUUID();
         return recipe;
     }
 
     private String name;
+    private UUID id;
     private int drawableId;
-
-
     private List<Ingredient> ingredients;
 
     public String getName() {
@@ -52,9 +53,13 @@ public class Recipe implements Parcelable {
         this.ingredients = ingredients;
     }
 
+    public UUID getId() {
+        return id;
+    }
 
     protected Recipe(Parcel in) {
         name = in.readString();
+        id = (UUID) in.readValue(UUID.class.getClassLoader());
         drawableId = in.readInt();
         if (in.readByte() == 0x01) {
             ingredients = new ArrayList<Ingredient>();
@@ -72,6 +77,7 @@ public class Recipe implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(name);
+        dest.writeValue(id);
         dest.writeInt(drawableId);
         if (ingredients == null) {
             dest.writeByte((byte) (0x00));
